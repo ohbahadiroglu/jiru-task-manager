fab24f15696d292c9c5de05a54442afa97199da5
canuludo <110036425+canuludo@users.noreply.github.com>
2022-08-09 14:54:35 +0300

    adam saatlik katmanlarının geliştirilmesi (#11)

--- backend/src/main/java/com/phexum/jira/controller/HourlyWageController.java
+++ backend/src/main/java/com/phexum/jira/controller/HourlyWageController.java
@@ -1,14 +1,13 @@
 package com.phexum.jira.controller;
 
 import com.phexum.jira.entity.HourlyWage;
-import com.phexum.jira.entity.Site;
+import com.phexum.jira.exception.NotFoundException;
 import com.phexum.jira.service.HourlyWageService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,14 +18,33 @@ public class HourlyWageController {
     public HourlyWageController(HourlyWageService hourlyWageService) {
         this.hourlyWageService = hourlyWageService;
     }
-    @PostMapping
-    public ResponseEntity create(@RequestBody HourlyWage hourlyWage) {
 
-        Optional<HourlyWage> op = Optional.ofNullable(hourlyWageService.create(hourlyWage));
+    @PostMapping
+    public ResponseEntity create(@Valid @RequestBody HourlyWage hourlyWage) {
+        return ResponseEntity.ok(hourlyWageService.create(hourlyWage));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<HourlyWage>> findAll() {
+        return ResponseEntity.ok(hourlyWageService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity findById(@PathVariable("id") Long id) {
+        Optional<HourlyWage> op = hourlyWageService.findById(id);
         if (op.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-
         return ResponseEntity.ok(op.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        try {
+            hourlyWageService.delete(id);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+        return ResponseEntity.ok().build();
     }
 }
--- backend/src/main/java/com/phexum/jira/entity/HourlyWage.java
+++ backend/src/main/java/com/phexum/jira/entity/HourlyWage.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -11,9 +14,11 @@ import java.util.List;
 @Setter
 public class HourlyWage {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank(message = "name should not be blank")
     private String name;
+    @NotNull(message = "amount should be given")
     private double amount;
 
 }
